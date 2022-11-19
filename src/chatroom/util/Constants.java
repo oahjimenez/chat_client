5204e8d79c8f2d9828068208c7e4143021d6262d
Oswaldo Jimenez <oswaldo-andres.jimenez-hidalgo@etu.univ-nantes.fr>
2022-11-19 13:35:11 +0100

    Add special channel pane

---
+++ src/chatroom/util/Constants.java
@@ -0,0 +1,19 @@
+package chatroom.util;
+
+public class Constants {
+
+	public static final String NEW_LINE = System.lineSeparator();
+	public static final String SINGLE_SPACE = " ";
+	public static final String EMPTY_STRING = "";
+
+	public static class Messages {
+
+		public static final String WELCOME_MESSAGE = "Welcome enter your name and press Start to begin\n";
+		public static final String INFINI_MESSAGE = "Write a number starting from number below up to infinity. Make sure it is not repeated within this chat!\n";
+		public static final String SPEAK_UP_MESSAGE = "Eager to share an important message? Take the mic and give a shout out! Nobody will interrump you until your message is sent.\n";
+		public static final String LOGOUT_MESSAGE = "Bye all, I am leaving";
+		public static final String CHANNEL_BEFORE_LOGIN_MESSAGE = "Login to get started";
+
+	}
+
+}
